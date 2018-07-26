538fcdfe3360108215fc5f519e716fe1ef3ea3a1
Chace Powell <cpcpowell8@gmail.com>
2018-07-25 23:42:56 -0400

    add question obj

---
+++ assets/javascript/app.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var question = $('#question');
+var answers = $('#answers');
+
+var questionObj = {
+    question1: {
+        question: '',
+        correct: '',
+        incorrect1: '',
+        incorrect2: '',
+        incorrect3: ''
+    },
+    question2: {
+        question: '',
+        correct: '',
+        incorrect1: '',
+        incorrect2: '',
+        incorrect3: ''
+    },
+    question3: {
+        question: '',
+        correct: '',
+        incorrect1: '',
+        incorrect2: '',
+        incorrect3: ''
+    },
+    question4: {
+        question: '',
+        correct: '',
+        incorrect1: '',
+        incorrect2: '',
+        incorrect3: ''
+    },
+    question5: {
+        question: '',
+        correct: '',
+        incorrect1: '',
+        incorrect2: '',
+        incorrect3: ''
+    },
+    question6: {
+        question: '',
+        correct: '',
+        incorrect1: '',
+        incorrect2: '',
+        incorrect3: ''
+    },
+    question7: {
+        question: '',
+        correct: '',
+        incorrect1: '',
+        incorrect2: '',
+        incorrect3: ''
+    },
+    question8: {
+        question: '',
+        correct: '',
+        incorrect1: '',
+        incorrect2: '',
+        incorrect3: ''
+    },
+    question9: {
+        question: '',
+        correct: '',
+        incorrect1: '',
+        incorrect2: '',
+        incorrect3: ''
+    },
+    question10: {
+        question: '',
+        correct: '',
+        incorrect1: '',
+        incorrect2: '',
+        incorrect3: ''
+    }
+}
