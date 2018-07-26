4a9940035382cef433a9c8a16205ad87d4633f64
Chace Powell <cpcpowell8@gmail.com>
2018-07-26 09:50:39 -0400

    refactor question object into an array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,75 +3,97 @@
 var question = $('#question');
 var answers = $('#answers');
 
-var questionObj = {
+var questionArr = [
+  {
     question1: {
-        question: '',
-        correct: '',
-        incorrect1: '',
-        incorrect2: '',
-        incorrect3: ''
-    },
-    question2: {
-        question: '',
-        correct: '',
-        incorrect1: '',
-        incorrect2: '',
-        incorrect3: ''
-    },
-    question3: {
-        question: '',
-        correct: '',
-        incorrect1: '',
-        incorrect2: '',
-        incorrect3: ''
-    },
-    question4: {
-        question: '',
-        correct: '',
-        incorrect1: '',
-        incorrect2: '',
-        incorrect3: ''
-    },
-    question5: {
-        question: '',
-        correct: '',
-        incorrect1: '',
-        incorrect2: '',
-        incorrect3: ''
-    },
-    question6: {
-        question: '',
-        correct: '',
-        incorrect1: '',
-        incorrect2: '',
-        incorrect3: ''
-    },
-    question7: {
-        question: '',
-        correct: '',
-        incorrect1: '',
-        incorrect2: '',
-        incorrect3: ''
-    },
-    question8: {
-        question: '',
-        correct: '',
-        incorrect1: '',
-        incorrect2: '',
-        incorrect3: ''
-    },
-    question9: {
-        question: '',
-        correct: '',
-        incorrect1: '',
-        incorrect2: '',
-        incorrect3: ''
-    },
-    question10: {
-        question: '',
-        correct: '',
-        incorrect1: '',
-        incorrect2: '',
-        incorrect3: ''
+      question: 'test',
+      correct: '',
+      incorrect1: '',
+      incorrect2: '',
+      incorrect3: ''
     }
-}
+  },
+  {
+    question2: {
+      question: '',
+      correct: '',
+      incorrect1: '',
+      incorrect2: '',
+      incorrect3: ''
+    }
+  },
+  {
+    question3: {
+      question: '',
+      correct: '',
+      incorrect1: '',
+      incorrect2: '',
+      incorrect3: ''
+    }
+  },
+  {
+    question4: {
+      question: '',
+      correct: '',
+      incorrect1: '',
+      incorrect2: '',
+      incorrect3: ''
+    }
+  },
+  {
+    question5: {
+      question: '',
+      correct: '',
+      incorrect1: '',
+      incorrect2: '',
+      incorrect3: ''
+    }
+  },
+  {
+    question6: {
+      question: '',
+      correct: '',
+      incorrect1: '',
+      incorrect2: '',
+      incorrect3: ''
+    }
+  },
+  {
+    question7: {
+      question: '',
+      correct: '',
+      incorrect1: '',
+      incorrect2: '',
+      incorrect3: ''
+    }
+  },
+  {
+    question8: {
+      question: '',
+      correct: '',
+      incorrect1: '',
+      incorrect2: '',
+      incorrect3: ''
+    }
+  },
+  {
+    question9: {
+      question: '',
+      correct: '',
+      incorrect1: '',
+      incorrect2: '',
+      incorrect3: ''
+    }
+  },
+  {
+    question10: {
+      question: '',
+      correct: '',
+      incorrect1: '',
+      incorrect2: '',
+      incorrect3: ''
+    }
+  }
+];
+
+console.log(questionArr[0].question1.question);
